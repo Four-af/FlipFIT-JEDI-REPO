77028b78b63fd59fb13b24529ff3e51f001f2c0a
anushka-Dahiya <anushkadahiya22@gmail.com>
2024-07-17 15:40:28 +0530

    customer update

--- JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/bean/Customer.java
+++ JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/bean/Customer.java
@@ -2,19 +2,35 @@ package com.flipkart.bean;
 
 public class Customer extends User {
 
-    private String customerID;
+    private String customerPhone;
+    private String cardDetails;
 
-    /**
-     * @return the customerID
-     */
-    public String getCustomerID() {
-        return customerID;
+    public Customer() {
     }
 
-    /**
-     * @param customerID the customerID to set
-     */
-    public void setCustomerID(String customerID) {
-        this.customerID = customerID;
+    public Customer(String userId, String userName, String email, String password, String customerPhone, String cardDetails) {
+        super(userId, userName, email, password, Role.CUSTOMER);
+        this.customerPhone = customerPhone;
+        this.cardDetails = cardDetails;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getCardDetails() { return cardDetails; }
+
+    public void setCardDetails(String cardDetails) { this.cardDetails = cardDetails; }
+
+    @Override
+    public String toString() {
+        return
+                super.toString()+
+                        "customerPhone='" + customerPhone + '\'' +
+                        ", cardDetails='" + cardDetails ;
     }
 }
