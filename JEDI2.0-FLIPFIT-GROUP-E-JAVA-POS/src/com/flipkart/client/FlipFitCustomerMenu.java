f002e8233bed46747f6c293f74dc4eb00941b14a
Four-af <aimzfatima08@gmail.com>
2024-07-17 16:08:20 +0530

    doing client

--- JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/client/FlipFitCustomerMenu.java
+++ JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/client/FlipFitCustomerMenu.java
@@ -1,5 +1,12 @@
 package com.flipkart.client;
 
+//imports
+import com.flipkart.bean.Booking;
+import com.flipkart.bean.Customer;
+import com.flipkart.bean.GymCenter;
+import com.flipkart.bean.Slot;
+
+
 public class FlipFitCustomerMenu {
     public static void getFlipFitCustomerMenu() {
         System.out.println("Welcome to FlipFit Customer Menu");
