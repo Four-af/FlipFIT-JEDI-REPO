6ccc26e15e63c1987f50d1c59f227c628c2a3044
anushka-Dahiya <anushkadahiya22@gmail.com>
2024-07-17 11:33:01 +0530

    User bean added

---
+++ JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/bean/User.java
@@ -0,0 +1,49 @@
+package com.flipkart.bean;
+
+public class User {
+
+    private String name;
+    private int userId;
+    private String emailId;
+    private String password;
+    private String roleId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+}
