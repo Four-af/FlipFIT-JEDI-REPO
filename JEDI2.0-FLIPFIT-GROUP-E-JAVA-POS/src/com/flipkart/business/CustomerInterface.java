5bccbda2d9e3db167cbdcb84a516ff08e8471e9f
anushka-Dahiya <anushkadahiya22@gmail.com>
2024-07-17 15:57:52 +0530

    customer bean and interface updated

--- JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/business/CustomerInterface.java
+++ JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/business/CustomerInterface.java
@@ -1,15 +1,23 @@
 package com.flipkart.business;
 
+import com.flipkart.bean.Booking;
+import com.flipkart.bean.Customer;
 import com.flipkart.bean.GymCenter;
-import java.util.HashMap;
+import com.flipkart.bean.Slot;
+import com.flipkart.utils.UserPlan;
+
+import java.sql.Date;
 import java.util.List;
 
-public interface CustomerInterface{
-    public void createCustomer(int userId, String name,String address);
-    public void editProfile(int userId, String name, String phoneNumber, String address);
-    public void deleteCustomer(int userId, String name);
-    public List<GymCenter> viewGyms();
-    public HashMap<String, Integer> viewSlots(int gymId, String date);
-    public void filterSlots();
+public interface CustomerInterface {
+    List<GymCenter> getAllGymCenterDetailsByCity(String city);
+    List<Slot> getAvailableSlots(String centreID, Date date);
+    List<Booking> getCustomerBookings(String customerId);
+    boolean bookSlot(String userID,Date date, String slotId,String centreId);
+    void cancelBookingByID(String bookingID);
+    void registerCustomer(String userName, String password, String email, String phoneNumber, String cardNumber);
+    Customer viewMyProfile(String userName);
+    boolean isUserValid(String userName, String password);
 
+    List<UserPlan> getCustomerPlan(String userName);
 }
