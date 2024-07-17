c958fba6241270b6782ed49c204d685db9f569e0
riyagupta2011 <riyagupta75751@gmail.com>
2024-07-17 10:48:08 +0530

    Adding admin in bean attribute

--- JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/bean/Admin.java
+++ JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/bean/Admin.java
@@ -1,4 +1,28 @@
 package com.flipkart.bean;
 
 public class Admin {
+    private String adminId;
+    private String userId;
+
+    public Admin(String adminId ,String userId) {
+        this.adminId = adminId;
+        this.userId = userId;
+    }
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
 }
