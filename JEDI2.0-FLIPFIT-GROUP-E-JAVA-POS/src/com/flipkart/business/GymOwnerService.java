bc6d7d4ae999f880c7afc9ac98d89e9b5023065a
Four-af <aimzfatima08@gmail.com>
2024-07-18 15:01:20 +0530

    GymOwnerService with DAO

--- JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/business/GymOwnerService.java
+++ JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/business/GymOwnerService.java
@@ -1,25 +1,25 @@
 package com.flipkart.business;
 
+import com.flipkart.dao.GymOwnerDAO;
 import com.flipkart.bean.GymOwner;
-import com.flipkart.bean.Role;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class GymOwnerService implements GymOwnerInterface {
-    List<GymOwner> allGymOwners = new ArrayList<>();
+    private static final GymOwnerDAO gymOwnerDAO = new GymOwnerDAO();
     public void requestGymOwnerApproval(String gymOwnerId) {
+        gymOwnerDAO.sendOwnerApprovalRequest(gymOwnerId);
     }
 
     public List<GymOwner> viewAllGymOwners() {
-        return allGymOwners;
+        return gymOwnerDAO.getGymOwnerList();
     }
 
     public boolean loginGymOwner(String username,String password){
-        return true;
+        return gymOwnerDAO.loginGymOwner(username,password);
     }
 
     public void registerGymOwner(String userId,String userName, String password, String email, String panNumber,String cardNumber) {
-        allGymOwners.add(new GymOwner(userId, userName, email, password, panNumber, cardNumber));
+        gymOwnerDAO.registerGymOwner(new GymOwner(userId,userName,email,password,panNumber,cardNumber));
     }
 }
