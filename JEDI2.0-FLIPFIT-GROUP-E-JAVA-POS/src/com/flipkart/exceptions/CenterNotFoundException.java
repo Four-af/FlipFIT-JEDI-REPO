b2f2d6978820f3e7cf0e817ba36ff4473a35ae61
anushka-Dahiya <anushkadahiya22@gmail.com>
2024-07-19 12:54:18 +0530

    exceptions added

---
+++ JEDI2.0-FLIPFIT-GROUP-E-JAVA-POS/src/com/flipkart/exceptions/CenterNotFoundException.java
@@ -0,0 +1,10 @@
+package com.flipkart.exceptions;
+
+import static com.flipkart.constants.Constants.RED_COLOR;
+import static com.flipkart.constants.Constants.RESET_COLOR;
+
+public class CenterNotFoundException extends RuntimeException{
+    public CenterNotFoundException(String gymId){
+        super(RED_COLOR+"Gym Centre" + gymId + " not found!"+RESET_COLOR);
+    }
+}
